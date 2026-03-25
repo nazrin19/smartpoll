2d7f4df9d6a7a7fe396b45d6d6ab673a8e1174aa
nazrin19 <nazreenabanu85@gmail.com>
2026-03-25 20:53:48 +0530

    Updated project

--- app.py
+++ app.py
@@ -1,83 +1,74 @@
-from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO, join_room, emit
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit, join_room
 import random
 import string
 
 app = Flask(__name__)
-app.secret_key = "smartpoll_secret"
+app.config['SECRET_KEY'] = 'poll-secret-123'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# This stores all rooms in memory
+# In-memory database
 rooms = {}
 
-# Generate a random 6-character room code
-def generate_code():
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-
-# Home page
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-# Host creates a room
-@app.route("/host")
+@app.route('/host')
 def host():
-    return render_template("host.html")
+    return render_template('host.html')
 
-# Guest joins a room
-@app.route("/vote")
+@app.route('/vote')
 def vote():
-    return render_template("vote.html")
+    return render_template('vote.html')
 
-# API: create a new room
-@app.route("/create_room", methods=["POST"])
+@app.route('/create_room', methods=['POST'])
 def create_room():
-    code = generate_code()
-    rooms[code] = {
-        "questions": [],
-        "current_q": 0,
-        "votes": {}
-    }
-    return {"code": code}
+    # Generates a 6-character code (e.g., AB1234)
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    rooms[code] = {'questions': [], 'votes': {}}
+    return jsonify({'code': code})
 
-# API: get room data
-@app.route("/room/<code>")
-def get_room(code):
+@app.route('/start_poll', methods=['POST'])
+def start_poll():
+    data = request.json
+    code = data.get('code')
     if code in rooms:
-        return rooms[code]
-    return {"error": "Room not found"}, 404
+        rooms[code]['questions'] = data.get('questions')
+        return jsonify({'status': 'success'})
+    return jsonify({'status': 'error'}), 404
 
-# SocketIO: host and guests join a room
-@socketio.on("join")
+# --- Socket Events ---
+
+@socketio.on('join')
 def on_join(data):
-    room = data["room"]
+    room = data['room']
     join_room(room)
-    emit("joined", {"room": room}, to=room)
 
-# SocketIO: guest submits a vote
-@socketio.on("vote")
-def on_vote(data):
-    room = data["room"]
-    answer = data["answer"]
-    if room in rooms:
-        votes = rooms[room]["votes"]
-        votes[answer] = votes.get(answer, 0) + 1
-        # Broadcast updated votes to everyone in the room
-        emit("update_votes", votes, to=room)
+@socketio.on('next_question')
+def handle_next(data):
+    room = data['room']
+    index = data.get('index', 0)
+    if room in rooms and index < len(rooms[room]['questions']):
+        question_data = rooms[room]['questions'][index]
+        rooms[room]['votes'] = {}  # Reset votes for the new question
+        emit('new_question', question_data, to=room)
 
-# SocketIO: host moves to next question
-@socketio.on("next_question")
-def on_next(data):
-    room = data["room"]
+@socketio.on('submit_vote')
+def handle_vote(data):
+    room = data.get('room')
+    answer = data.get('answer')
+    next_idx = data.get('next_index') # Get the branched path index
+    
     if room in rooms:
-        rooms[room]["current_q"] += 1
-        q_index = rooms[room]["current_q"]
-        questions = rooms[room]["questions"]
-        if q_index < len(questions):
-            rooms[room]["votes"] = {}
-            emit("new_question", questions[q_index], to=room)
+        # 1. Update the Host's Live View
+        rooms[room]['votes'][answer] = rooms[room]['votes'].get(answer, 0) + 1
+        emit('update_votes', rooms[room]['votes'], to=room)
+
+        # 2. Send the NEXT question ONLY to this specific user
+        if next_idx is not None and next_idx < len(rooms[room]['questions']):
+            next_q = rooms[room]['questions'][next_idx]
+            emit('new_question', next_q, room=request.sid)
         else:
-            emit("poll_ended", {}, to=room)
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True)
+            # If no next question, tell them they are done
+            emit('new_question', None, room=request.sid)
